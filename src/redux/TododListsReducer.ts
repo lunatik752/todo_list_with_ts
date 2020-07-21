dbf9c0ec0754e64b33afc4c38cc0df1abe427020
lunatik752 <whitemaxim752@gmail.com>
2020-07-21 16:15:33 +0300

    added api (authApi)

---
+++ src/redux/TododListsReducer.ts
@@ -0,0 +1,24 @@
+import {TodoListType} from "../types/entities";
+
+
+
+
+
+type InitialStateType = {
+    todoLists: Array<TodoListType>
+}
+
+
+
+
+
+//action types
+
+
+// actions
+
+
+
+
+
+//Thunk
---
+++ src/types/entities.ts
@@ -0,0 +1,21 @@
+export type TodoListType = {
+    id: string
+    title: string,
+    addedDate: string,
+    order: number
+    tasks: Array<TaskType>
+}
+
+export type TaskType = {
+    description: string
+    title: string
+    completed: boolean
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+    id: string
+    todoListId: string
+    order: number
+    addedDate: string
+}
