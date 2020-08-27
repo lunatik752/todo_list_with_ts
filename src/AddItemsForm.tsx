77354c0cde420296745ace69c410ee42ebd23306
lunatik752 <whitemaxim752@gmail.com>
2020-08-27 13:06:48 +0300

    added component AddItemsForm  (lesson 06)

---
+++ src/AddItemsForm.tsx
@@ -0,0 +1,44 @@
+import React, {ChangeEvent, useState, KeyboardEvent} from "react";
+
+
+type AddItemPropsType = {
+    addItem: (title: string) => void
+}
+
+function AddItem(props: AddItemPropsType) {
+
+    let [title, setTitle] = useState('');
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== '') {
+            props.addItem(title);
+            setTitle('')
+        } else {
+            setError('Title is required')
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null)
+        if (e.charCode === 13) {
+            addItem()
+        }
+    }
+    let inputClassName = error ? 'error' : '';
+
+    return (
+        <div>
+            <input className={inputClassName}
+                   value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}/>
+            <button onClick={addItem}>+</button>
+            {error && <div className={'error-message'}>{error}</div>}
+        </div>)
+
+}
