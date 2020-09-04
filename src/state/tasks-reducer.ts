0dc99e8e1b87fb89034f719e9a80bd57ea663887
lunatik752 <whitemaxim752@gmail.com>
2020-09-04 11:03:53 +0300

    added  added deletion and addition of tasks (lesson 09)

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,43 +1,55 @@
 import {TasksStateType} from "../App";
+import {v1} from "uuid";
 
 
 
-type ActionType = SomeActionType1 | SomeActionType2
+type ActionType = RemoveTaskActionType | AddTaskActionType
 
 
 
-export type SomeActionType1 = {
-    type: '',
-    id: string
+export type RemoveTaskActionType = {
+    type: 'REMOVE-TASK',
+    id: string,
+    todoListId: string
 }
 
-export type SomeActionType2 = {
-    type: '',
-    id: string
+export type AddTaskActionType = {
+    type: 'ADD-TASK',
+    title: string,
+    todoListId: string
 }
 
 
-export const todoListReducer = (state: TasksStateType, action: ActionType) => {
+export const tasksReducer = (state: TasksStateType, action: ActionType) => {
     switch (action.type) {
-        case'': {
-            return state
+        case 'REMOVE-TASK': {
+            let todoListTasks = state[action.todoListId];
+            state[action.todoListId] = todoListTasks.filter(task => task.id !== action.id)
+            return {...state}
         }
-
+        case "ADD-TASK": {
+            let task = {id: v1(), title: action.title, isDone: false};
+            let todoListTasks = state[action.todoListId];
+            state[action.todoListId] = [task, ...todoListTasks]
+            return {...state}
+        }
         default:
             throw new Error("I don't understand this type")
     }
 }
 
-export const RemoveTodoListAC = (taskId: string): SomeActionType1 => {
+export const removeTaskAC = (taskId: string, todoListId: string): RemoveTaskActionType => {
     return {
-        type: '',
-        id: taskId
+        type: 'REMOVE-TASK',
+        id: taskId,
+        todoListId: todoListId
     }
 }
 
-export const AddTodoListAC = (taskId: string): SomeActionType2 => {
+export const addTaskAC = (title: string, todoListId: string): AddTaskActionType => {
     return {
-        type: '',
-        id: taskId
+        type: 'ADD-TASK',
+        title: title,
+        todoListId: todoListId
     }
 }
