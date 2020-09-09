8bfbcb73b1e13da65687703160037a0a28992009
lunatik752 <whitemaxim752@gmail.com>
2020-09-09 19:29:14 +0300

    add AppWithRedux.stories.tsx (lesson 12)

---
+++ src/AppWithRedux.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppWithRedux from "./AppWithRedux";
+
+
+export default  {
+    title: 'AppWithRedux Stories',
+    component: AppWithRedux
+}
+
+export const AppWithReduxBaseExample = (props: any) => {
+    return <AppWithRedux/>
+}
