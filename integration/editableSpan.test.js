9b3c41c7317f998a8531af5ec1aea88809e5a3c5
lunatik752 <whitemaxim752@gmail.com>
2020-09-10 16:29:48 +0300

    add editableSpan.test.js (lesson 12)

---
+++ integration/editableSpan.test.js
@@ -0,0 +1,11 @@
+describe('editableSpan', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        // eslint-disable-next-line no-undef
+        await page.goto('http://localhost:9009/iframe.html?id=example-editablespan--editable-span-base-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
