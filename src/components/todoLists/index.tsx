fd6231a38956505cdb9520238c1c0043eff72ac7
lunatik752 <whitemaxim752@gmail.com>
2020-10-14 15:34:02 +0300

    added todoListActions to index.tsx (lesson 20)

--- src/components/todoLists/index.tsx
+++ src/components/todoLists/index.tsx
@@ -1,5 +1,11 @@
 import * as todoListsSelectors from './selectors'
+import * as todoListActions from './todoList-actions'
+
 
 export  {
     todoListsSelectors
+}
+
+export {
+    todoListActions
 }
