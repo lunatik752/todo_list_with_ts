2d9803a1542c928599f7be96e7259b094708797a
lunatik752 <whitemaxim752@gmail.com>
2020-10-14 15:19:53 +0300

    added hook useActions to store.ts (lesson 20)

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,4 @@
-import {combineReducers} from "redux";
+import {ActionCreatorsMapObject, bindActionCreators, combineReducers} from "redux";
 import {tasksReducer} from "../components/task/tasks-reducer";
 import {todoListReducer} from "../components/todoLists/todoList-reducer";
 import thunk from "redux-thunk";
@@ -6,6 +6,7 @@ import {appReducer} from "../components/app/app-reducer";
 import {authReducer} from "../features/login/auth-reducer";
 import {configureStore} from "@reduxjs/toolkit";
 import {useDispatch} from "react-redux";
+import { useMemo } from "react";
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
@@ -33,3 +34,12 @@ window.store = store;
 
 type  AppDispatchType = typeof store.dispatch
 export const  useAppDispatch = () => useDispatch<AppDispatchType>()
+
+
+export function useActions<T extends ActionCreatorsMapObject<any>>(actions: T) {
+    const dispatch = useDispatch()
+
+
+const boundActions = useMemo(() => {
+    return bindActionCreators(actions, dispatch)
+}, [actions, dispatch])}
