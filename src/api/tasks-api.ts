0148906e3ce9e02e2d2c757fc1a0c0adb9ae870d
lunatik752 <whitemaxim752@gmail.com>
2020-10-26 23:10:47 +0300

    fixed tasks-api.ts (lesson 20)

--- src/api/tasks-api.ts
+++ src/api/tasks-api.ts
@@ -4,16 +4,16 @@ import {instance} from "./instance";
 
 export const tasksAPI = {
     getTasks(todoListId: string) {
-        return instance.get<GetTasksResponseType>(`${todoListId}/tasks`)
+        return instance.get<GetTasksResponseType>(`todo-lists/${todoListId}/tasks`)
     },
     createTask(todoListId: string, title: string) {
-        return instance.post<ResponseType<{ item: TaskType }>>(`${todoListId}/tasks`, {title})
+        return instance.post<ResponseType<{ item: TaskType }>>(`todo-lists/${todoListId}/tasks`, {title})
     },
     deleteTask(todoListId: string, taskId: string) {
-        return instance.delete<ResponseType>(`${todoListId}/tasks/${taskId}`)
+        return instance.delete<ResponseType>(`todo-lists/${todoListId}/tasks/${taskId}`)
     },
 
     updateTask(todolistId: string, taskId: string, modelTask: UpdateModelTaskType) {
-        return instance.put<ResponseType>(`${todolistId}/tasks/${taskId}`, modelTask)
+        return instance.put<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`, modelTask)
     }
 }
