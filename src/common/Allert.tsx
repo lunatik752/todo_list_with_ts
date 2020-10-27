d91c855d02685a896d4ce9da0cb6fa3f349e37b7
lunatik752 <whitemaxim752@gmail.com>
2020-10-27 15:28:22 +0300

    changer Alert.tsx (lesson 20)

--- src/common/Allert.tsx
+++ src/common/Allert.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Snackbar from '@material-ui/core/Snackbar'
 import MuiAlert, {AlertProps} from '@material-ui/lab/Alert'
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {AppRootStateType} from "../state/store";
-import {RequestErrorType, setAppError} from "../components/app/app-reducer";
+import {RequestErrorType} from "../components/app/app-reducer";
+import {useActions} from "../utils/redux-utils";
+import {appActions} from '../features/CommonActions/App';
 
 
 function Alert(props: AlertProps) {
@@ -12,15 +14,14 @@ function Alert(props: AlertProps) {
 
 export function ErrorSnackbar() {
 
-    const error = useSelector<AppRootStateType, RequestErrorType>( state => state.app.error)
-    const dispatch = useDispatch()
-
+    const error = useSelector<AppRootStateType, RequestErrorType>(state => state.app.error)
+    const {setAppError} = useActions(appActions)
 
     const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
         if (reason === 'clickaway') {
 
         }
-        dispatch(setAppError({error:null}))
+        setAppError({error: null})
     }
 
     return (
