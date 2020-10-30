a6d365db871e80b9ce160c90a5842f8bd26e87db
lunatik752 <whitemaxim752@gmail.com>
2020-10-30 15:45:33 +0300

    changed Task.tsx (lesson 20)

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,8 +1,7 @@
 import React, {ChangeEvent, useCallback} from "react";
 import {EditableSpan} from "../../common/EditableSpan";
-import {IconButton} from "@material-ui/core";
+import {Checkbox, IconButton} from "@material-ui/core";
 import {Delete} from '@material-ui/icons';
-import Checkbox from '@material-ui/core/Checkbox';
 import {TaskDomainType} from "./tasks-reducer";
 import {tasksActions} from "./index";
 import {useActions} from "../../utils/redux-utils";
@@ -14,24 +13,27 @@ type PropsTaskType = {
     todoListId: string,
 }
 
-export const Task = React.memo(function (props: PropsTaskType) {
+export const Task = React.memo( (props: PropsTaskType) => {
 
         const {updateTask, removeTask} = useActions(tasksActions)
 
-        const onClickHandler =  useCallback(() => removeTask({taskId: props.task.id, todoListId: props.todoListId}),
+        const onClickHandler = useCallback(() => removeTask({taskId: props.task.id, todoListId: props.todoListId}),
             [removeTask, props.task.id, props.todoListId])
 
-        const onChangeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            let newIsDoneValue = e.currentTarget.checked;
-            updateTask({taskId: props.task.id,
-            domainModel: {status: newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New},
-            todoListId: props.todoListId})
-        }
+        const onChangeStatusHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+            updateTask({
+                taskId: props.task.id,
+                domainModel: {status: e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New},
+                todoListId: props.todoListId
+            })
+        }, [updateTask, props.task.id, props.todoListId])
 
         const onChangeTitleHandler = useCallback((newtTitle: string) => {
-            updateTask({taskId: props.task.id,
+            updateTask({
+                taskId: props.task.id,
                 domainModel: {title: newtTitle},
-                todoListId: props.todoListId})
+                todoListId: props.todoListId
+            })
         }, [updateTask, props.task.id, props.todoListId])
 
         const disabled = props.task.entityTaskStatus === 'loading'
@@ -42,14 +44,15 @@ export const Task = React.memo(function (props: PropsTaskType) {
                     style={{position: 'relative', display: 'flex', alignItems: 'center'}}>
             <Checkbox color={"primary"}
                       checked={props.task.status === TaskStatuses.Completed}
-                      onChange={onChangeStatusHandler} disabled={disabled}
+                      onChange={onChangeStatusHandler}
+                      disabled={disabled}
             />
             <EditableSpan title={props.task.title}
                           onChangeTitle={onChangeTitleHandler}
                           disabled={disabled}
-                          />
+            />
             <IconButton onClick={onClickHandler} disabled={disabled} style={{position: 'absolute', right: '5px'}}>
-                <Delete/>
+                <Delete fontSize={'small'}/>
             </IconButton>
         </div>
     }
