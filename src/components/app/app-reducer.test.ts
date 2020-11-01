f97fdeb272c4c3b6ba4005bce4704dd107c45dd2
lunatik752 <whitemaxim752@gmail.com>
2020-11-01 20:11:24 +0300

    fixed app-reducer.test.ts (lesson 20)

--- src/components/app/app-reducer.test.ts
+++ src/components/app/app-reducer.test.ts
@@ -1,6 +1,9 @@
-import {appReducer, InitialAppReducerStateType, initializeApp, setAppError, setAppStatus} from "./app-reducer";
+import {asyncActions as appAsyncActions, InitialAppReducerStateType} from "./app-reducer";
+import {appReducer} from "./index";
+import {appActions} from "../../features/CommonActions/App";
 
 let startState: InitialAppReducerStateType;
+const {initializeApp} = appAsyncActions
 
 beforeEach(() => {
     startState = {
@@ -14,7 +17,7 @@ test('error should be changed', () => {
 
     const error = 'some error';
 
-    const action = setAppError({error: error});
+    const action = appActions.setAppError({error: error});
     const endState = appReducer(startState, action)
 
     expect(endState.error).toBe(error);
@@ -23,7 +26,7 @@ test('error should be changed', () => {
 
 test('status should be changed', () => {
 
-    const action = setAppStatus({status: "loading"});
+    const action = appActions.setAppStatus({status: "loading"});
     const endState = appReducer(startState, action)
 
     expect(endState.status).toBe('loading');
@@ -31,7 +34,7 @@ test('status should be changed', () => {
 
 test('isInitialized should be changed', () => {
 
-    const action = initializeApp.fulfilled(undefined, '',undefined);
+    const action = initializeApp.fulfilled(undefined, '', undefined);
     const endState = appReducer(startState, action)
 
     expect(endState.isInitialized).toBe(true);
